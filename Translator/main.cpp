bc833a8155504febdfef4dae6995acd9de9c65cf
kyrillWhite <kiribel2000@gmail.com>
2022-03-06 00:56:44 +0500

    refactor: added throw error

--- Translator/main.cpp
+++ Translator/main.cpp
@@ -9,18 +9,19 @@ int main()
     setlocale(LC_CTYPE, "Russian");
     ifstream input("input.txt");
     //ofstream output("output.txt");
-    auto IO = make_unique<CIO>(input);
+    auto IO = make_unique<CIO>(input, cout);
     auto lexer = make_unique<CLexer>(move(IO));
     
-    auto token = lexer->GetNextToken();
-    while (token->GetType() != ttEmpty) {
-        cout << token->ToString() << endl;
-        token = lexer->GetNextToken();
+    try {
+        while (!lexer->GetIOPtr()->IsEndOfInput()) {
+            auto token = lexer->GetNextToken();
+            cout << token->ToString() << endl;
+        }
     }
-    lexer->GetIOPtr()->PrintErrors();
+    catch (CError* error) {
+        lexer->GetIOPtr()->PrintErrors();
+    }
+
     input.close();
-    
-    string res = lexer->GetIOPtr()->GetOutputString();
-    auto aa = res.c_str();
     //output.close();
 }
